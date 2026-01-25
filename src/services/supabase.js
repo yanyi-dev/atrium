0c8a4c9882c7e899e4f3a57fa052be2754da4e89
yanyi <3115654781@qq.com>
2026-01-25 12:39:20 +0800

    feat: 项目数据初始化，配置本地环境变量文件

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://dhdwkpjnkhsugchotwny.supabase.co";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 // const supabaseKey = process.env.SUPABASE_KEY;
 // const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoZHdrcGpua2hzdWdjaG90d255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NjEzODgsImV4cCI6MjA4MTUzNzM4OH0.yD1sMzs8te7Y13RuEFsLTJ6OyEbl1X1ivqxfwaHTOLI";
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
