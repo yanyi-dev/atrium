99ec626d91cacbafb6ef13452f3affa12fe265dc
yanyi <3115654781@qq.com>
2026-01-29 16:33:19 +0800

    feat: 在登陆页面提供测试账密，方便应用测试

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("test@test.com");
+  const [password, setPassword] = useState("12456389");
   const { login, isLogining } = useLogin();
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,23 +1,24 @@
 import styled from "styled-components";
-import { useDarkMode } from "../context/DarkModeContext";
+// import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
   text-align: center;
 `;
 
-const Img = styled.img`
-  height: 9.6rem;
-  width: auto;
-`;
+// const Img = styled.img`
+//   height: 9.6rem;
+//   width: auto;
+// `;
 
 function Logo() {
-  const { isDarkMode } = useDarkMode();
+  // const { isDarkMode } = useDarkMode();
 
-  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
+  // const src = isDarkMode ? "/(深色logo名).png" : "/(浅色logo名).png";
 
   return (
     <StyledLogo>
-      <Img src={src} alt="Logo" />
+      <h1>Atrium</h1>
+      <h3>雅庭旅宿管理系统</h3>
     </StyledLogo>
   );
 }
